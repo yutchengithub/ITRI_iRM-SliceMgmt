7634fcec09e6cf8979bb1a42b26240ad4894275e
yutchengithub <victzeng@itri.org.tw>
2023-12-21 10:32:09 +0800

    Delete the API key
    Delete real API key to prevent security risks.
    
    刪除真實 API 金鑰以防範安全風險。

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: true, // 這個標誌用於指示應用是否運行在"生產"模式
 
   // 這表示這是用於"生產"環境的 Google Maps API 密鑰
-  googleMapsApiKey: 'AIzaSyA9dNTx8wzfyAFQW0_9HJmlAdVYCWzZPLY', // @12/20 Add the Google Maps API Key of yuchen
-};
+  googleMapsApiKey: 'Google_Maps_API_KEY', // 請自行調整成自己的KEY @12/20 Add 
+}
