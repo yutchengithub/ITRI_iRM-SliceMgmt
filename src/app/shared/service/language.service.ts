2664618c83f4cab43b9f42fccb1df86f0a14ad4c
yutchengithub <victzeng@itri.org.tw>
2024-01-24 21:51:06 +0800

    功能更新：依據使用者語系切換樣式表
    功能更新：依據使用者語系切換樣式表
    
    本次提交新增了一個功能，能夠根據用戶選擇的語言動態切換CSS樣式表。修改包括：
    - 在LanguageService中新增changeLanguageStylesheet方法。
    - 更新setLanguage方法以在語言變更時調用changeLanguageStylesheet。
    - 在MainComponent的ngOnInit中增加初始樣式表加載邏輯。
    
    這確保了應用的樣式能與所選語言保持一致性。
    
    Feature Update: Switch Stylesheets Based on User Language Selection
    
    This commit introduces a feature that dynamically switches the CSS stylesheets according to the user's selected language. Changes include:
    - Added changeLanguageStylesheet method in LanguageService.
    - Updated setLanguage method to invoke changeLanguageStylesheet upon language change.
    - Added logic for initial stylesheet loading in MainComponent's ngOnInit.
    
    This ensures that the application's styling remains consistent with the chosen language.

--- src/app/shared/service/language.service.ts
+++ src/app/shared/service/language.service.ts
@@ -20,6 +20,16 @@ export class LanguageService {
     this.setLanguage();
   }
 
+  // setLanguage() {
+  //   window.sessionStorage.setItem('language', this.language);
+  //   if (this.language === 'EN') {
+  //     this.i18n = Enlanguage;
+  //   } else {
+  //     this.i18n = TwLanguage;
+  //   }
+  // }
+
+  // @2024/01/24 Update
   setLanguage() {
     window.sessionStorage.setItem('language', this.language);
     if (this.language === 'EN') {
@@ -27,6 +37,9 @@ export class LanguageService {
     } else {
       this.i18n = TwLanguage;
     }
+    
+    // 當語言變化時，更新樣式表
+    this.changeLanguageStylesheet( this.language );
   }
 
   changeI18n(language: string) {
@@ -39,6 +52,37 @@ export class LanguageService {
       this.changeI18n('EN');
     } else {
       this.changeI18n('TW');
+    }
+  }
+
+  // @2024/01/24 Add
+  // 函數: 切換語言樣式表
+  changeLanguageStylesheet(language: string) {
+    // 在控制台打印當前語言樣式表的訊息
+    console.log("The current Language Style is styles." + language.toLowerCase() + ".css");
+
+    // 獲取HTML文檔中的<head>元素
+    const head = document.getElementsByTagName('head')[0];
+    // 嘗試獲取頁面上已存在的用於語言樣式的<link>元素
+    const existingLinkElement = document.getElementById('language-stylesheet') as HTMLLinkElement;
+
+    // 檢查是否已經存在語言樣式的<link>元素
+    if (existingLinkElement) {
+      // 如果存在，則更新該<link>元素的href屬性，指向新的樣式表文件
+      existingLinkElement.href = `assets/css/styles.${language.toLowerCase()}.css`;
+    } else {
+      // 如果不存在，則創建新的<link>元素
+      const linkElement = document.createElement('link');
+      // 設置新<link>元素的id，以便將來可以識別和選擇它
+      linkElement.id = 'language-stylesheet';
+      // 設置<link>元素的rel屬性，表明它是一個樣式表
+      linkElement.rel = 'stylesheet';
+      // 設置<link>元素的type屬性，表明它的類型是文字/樣式表
+      linkElement.type = 'text/css';
+      // 設置<link>元素的href屬性，指向對應語言的樣式表文件
+      linkElement.href = `assets/css/styles.${language.toLowerCase()}.css`;
+      // 將新創建的<link>元素加入到<head>元素中，使其生效
+      head.appendChild(linkElement);
     }
   }
 
