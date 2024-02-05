242de12a97edd0a3fe52eb20a29b01e1bc0cdb4f
yutchengithub <victzeng@itri.org.tw>
2024-02-05 17:15:04 +0800

    更新 .gitignore
    更新 .gitignore 檔案至不可 clone
    environment.ts
    src\environments\environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   production: true, // 這個標誌用於指示應用是否運行在"生產"模式
 
   // 這表示這是用於"生產"環境的 Google Maps API 密鑰
-  googleMapsApiKey: 'AIzaSyCVtv-qR0yb1vTZd--r2IimkftRGvhNFb4', // 請自行調整成自己的KEY @12/20 Add 
+  googleMapsApiKey: 'Google_Maps_API_KEY', // 請自行調整成自己的KEY @12/20 Add 
 }
