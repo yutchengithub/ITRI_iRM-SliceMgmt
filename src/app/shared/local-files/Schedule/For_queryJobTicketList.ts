28ec36df2185e6fde3b4308a5e750b10d63ac896
yutchengithub <130173760+yutchengithub@users.noreply.github.com>
2024-03-18 02:54:43 +0800

    Schedule Mgmt 更新
    1. html 上新增判定排程類別機制
    2. 中英語言檔 - 新增排程 5 種類別名稱
    3. 調整排程列表 local files 以便於顯示

--- src/app/shared/local-files/Schedule/For_queryJobTicketList.ts
+++ src/app/shared/local-files/Schedule/For_queryJobTicketList.ts
@@ -14,7 +14,7 @@
             "tickettype": "0",
             "ticketstatus": "0",
             "executedtype": "0",
-            "executedtime": "2024-03-15 19:00:00",
+            "executedtime": "2024-03-09 19:00:00",
             "jobticket": "0",
             "ticketresult": ""
           },
@@ -22,9 +22,9 @@
             "id": "3fadc30352144dc8a6d4",
             "fieldid": "aa520ec8e5074f54b77d",
             "tickettype": "2",
-            "ticketstatus": "0",
+            "ticketstatus": "1",
             "executedtype": "1",
-            "executedtime": "2024-03-15 13:00:00",
+            "executedtime": "2024-03-16 13:00:00",
             "jobticket": "0",
             "ticketresult": ""
           },
@@ -32,9 +32,9 @@
             "id": "7edf1bea0919445eb89d",
             "fieldid": "aa520ec8e5074f54b77d",
             "tickettype": "4",
-            "ticketstatus": "0",
+            "ticketstatus": "2",
             "executedtype": "1",
-            "executedtime": "2024-03-15 10:00:00",
+            "executedtime": "2024-03-14 10:00:00",
             "jobticket": "0",
             "ticketresult": ""
           },
@@ -42,7 +42,7 @@
             "id": "f7259488c98645bebf7e",
             "fieldid": "aa520ec8e5074f54b77d",
             "tickettype": "3",
-            "ticketstatus": "0",
+            "ticketstatus": "1",
             "executedtype": "0",
             "executedtime": "2024-03-15 10:00:00",
             "jobticket": "0",
@@ -54,7 +54,7 @@
             "tickettype": "1",
             "ticketstatus": "0",
             "executedtype": "0",
-            "executedtime": "2024-03-15 09:35:00",
+            "executedtime": "2024-03-11 09:35:00",
             "jobticket": "0",
             "ticketresult": ""
           },
@@ -62,9 +62,9 @@
             "id": "af0c1fc4c90b4205a3ca",
             "fieldid": "f769da087f2044e7a0d7",
             "tickettype": "2",
-            "ticketstatus": "0",
+            "ticketstatus": "2",
             "executedtype": "0",
-            "executedtime": "2024-03-15 11:40:00",
+            "executedtime": "2024-03-13 11:40:00",
             "jobticket": "0",
             "ticketresult": ""
           }
