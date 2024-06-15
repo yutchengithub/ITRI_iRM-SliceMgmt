d5f8567eeb1a11825898fa534222137251f60c1b
yutchengithub <130173760+yutchengithub@users.noreply.github.com>
2024-06-16 04:28:30 +0800

    故障管理 - 新增三個 api (  取得場域告警、取得基站告警、取得基站網元告警 )

--- src/app/shared/api/For_Fault_Mgmt.ts
+++ src/app/shared/api/For_Fault_Mgmt.ts
@@ -18,7 +18,8 @@ export class apiForFaultMgmt {
   restPath = this.commonService.restPath;         // Get the root path 
   //sessionId = this.commonService.getSessionId();  // Get the Session ID 
 
-  // 取得所有告警資訊用 @2024/06/03 Add
+  // @2024/06/03 Add
+  // 取得 "系統" 所有告警資訊用
   queryCurrentAllFaultMessage( params: any ): Observable<any> {
 
     // 每次調用 API 時都動態獲取最新的 sessionId
@@ -26,6 +27,48 @@ export class apiForFaultMgmt {
 
     const url = `${this.restPath}/queryCurrentAllFaultMessage/${sessionId}`;
     return this.http.get( url, { params } );  // 將參數物件傳遞給 HTTP GET 請求
+  }
+
+  // @2024/06/16 Add
+  // 取得 "指定場域" 所有告警資訊用 @2024/06/16 Add
+  queryCurrentFieldFaultMessage( fieldId: string, params: any ): Observable< any > {
+  
+    // 每次調用 API 時都動態獲取最新的 sessionId
+    const sessionId = this.commonService.getSessionId();
+
+    // 組合 API 的 URL，包含 session ID 和場域 ID
+    const url = `${this.restPath}/queryCurrentFieldFaultMessage/${sessionId}/${fieldId}`;
+
+    // 發送 HTTP GET 請求到指定的 URL，並將 params 物件作為查詢參數傳遞給請求
+    return this.http.get( url, { params } );
+  }
+
+  // @2024/06/16 Add
+  // 取得 "指定基站" 所有告警資訊用 @2024/06/16 Add
+  queryCurrentBsFaultMessage( bsId: string, params: any ): Observable< any > {
+  
+    // 每次調用 API 時都動態獲取最新的 sessionId
+    const sessionId = this.commonService.getSessionId();
+
+    // 組合 API 的 URL，包含 session ID 和場域 ID
+    const url = `${this.restPath}/queryCurrentBsFaultMessage/${sessionId}/${bsId}`;
+
+    // 發送 HTTP GET 請求到指定的 URL，並將 params 物件作為查詢參數傳遞給請求
+    return this.http.get( url, { params } );
+  }
+
+  // @2024/06/16 Add
+  // 取得 "指定基站網元" 所有告警資訊用
+  queryCurrentBsComFaultMessage( neId: string, params: any ): Observable< any > {
+
+    // 每次調用 API 時都動態獲取最新的 sessionId
+    const sessionId = this.commonService.getSessionId();
+
+    // 組合 API 的 URL，包含 session ID 和場域 ID
+    const url = `${this.restPath}/queryCurrentBsFaultMessage/${sessionId}/${neId}`;
+
+    // 發送 HTTP GET 請求到指定的 URL，並將 params 物件作為查詢參數傳遞給請求
+    return this.http.get( url, { params } );
   }
 
 }
