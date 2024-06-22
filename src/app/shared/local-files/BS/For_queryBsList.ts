1c975043ebffab634992289242584fefaa3d57fc
yutchengithub <victzeng@itri.org.tw>
2024-06-22 16:04:44 +0800

    補上 queryBsList local 補上20筆未被任何場域使用的基站數據

--- src/app/shared/local-files/BS/For_queryBsList.ts
+++ src/app/shared/local-files/BS/For_queryBsList.ts
@@ -324,6 +324,405 @@
             "adminstate": 1,
             "laston": "2024-04-02 18:53:04.530114",
             "lastoff": null
+          },
+          {
+            "id": "7b2c9f1a3e5d8g6h4i2j",
+            "name": "BS-north-1",
+            "ip": "",
+            "port": "",
+            "position": "[121.523687,25.037964]",
+            "description": "bs-1",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "1a2b3c4d5e6f7g8h9i0j"
+              }
+            ],
+            "status": 1,
+            "gNBId": 1,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-01 09:15:32.123456",
+            "lastoff": null
+          },
+          {
+            "id": "3k5m7n9p1q3r5s7t9u1v",
+            "name": "BS-south-3",
+            "ip": "",
+            "port": "",
+            "position": "[120.646912,23.475321]",
+            "description": "bs-3",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "2b4d6f8h0j2l4n6p8r0t"
+              }
+            ],
+            "status": 2,
+            "gNBId": 3,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-03 14:27:58.987654",
+            "lastoff": null
+          },
+          {
+            "id": "2w4y6a8c0e2g4i6k8m0o",
+            "name": "BS-east-4",
+            "ip": "",
+            "port": "",
+            "position": "[121.874569,24.163785]",
+            "description": "bs-4",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "3c5e7g9i1k3m5o7q9s1u"
+              }
+            ],
+            "status": 1,
+            "gNBId": 4,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-04 11:42:17.246810",
+            "lastoff": null
+          },
+          {
+            "id": "9p1q3r5s7t9u1v3w5y7a",
+            "name": "BS-west-5",
+            "ip": "",
+            "port": "",
+            "position": "[120.198765,23.987654]",
+            "description": "bs-5",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "4d6f8h0j2l4n6p8r0t2v"
+              }
+            ],
+            "status": 2,
+            "gNBId": 5,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-05 16:33:09.135791",
+            "lastoff": null
+          },
+          {
+            "id": "8b0d2f4h6j8l0n2p4r6t",
+            "name": "BS-central-6",
+            "ip": "",
+            "port": "",
+            "position": "[120.987654,23.456789]",
+            "description": "bs-6",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "5e7g9i1k3m5o7q9s1u3w"
+              }
+            ],
+            "status": 1,
+            "gNBId": 6,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-06 08:59:43.975310",
+            "lastoff": null
+          },
+          {
+            "id": "7a9c1e3g5i7k9m1o3q5s",
+            "name": "BS-northwest-7",
+            "ip": "",
+            "port": "",
+            "position": "[121.345678,25.234567]",
+            "description": "bs-7",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "6f8h0j2l4n6p8r0t2v4x"
+              }
+            ],
+            "status": 2,
+            "gNBId": 7,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-07 13:21:56.864209",
+            "lastoff": null
+          },
+          {
+            "id": "6t8u0w2y4a6c8e0g2i4k",
+            "name": "BS-northeast-8",
+            "ip": "",
+            "port": "",
+            "position": "[121.789012,24.901234]",
+            "description": "bs-8",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "7g9i1k3m5o7q9s1u3w5y"
+              }
+            ],
+            "status": 1,
+            "gNBId": 8,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-08 10:05:37.753108",
+            "lastoff": null
+          },
+          {
+            "id": "5l7n9p1r3t5v7x9z1b3d",
+            "name": "BS-southwest-9",
+            "ip": "",
+            "port": "",
+            "position": "[120.567890,23.210987]",
+            "description": "bs-9",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "8h0j2l4n6p8r0t2v4x6z"
+              }
+            ],
+            "status": 2,
+            "gNBId": 9,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-09 17:48:22.642007",
+            "lastoff": null
+          },
+          {
+            "id": "4f6h8j0l2n4p6r8t0v2x",
+            "name": "BS-southeast-10",
+            "ip": "",
+            "port": "",
+            "position": "[121.123456,23.789012]",
+            "description": "bs-10",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "9i1k3m5o7q9s1u3w5y7a"
+              }
+            ],
+            "status": 1,
+            "gNBId": 10,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-10 07:39:14.531906",
+            "lastoff": null
+          },
+          {
+            "id": "3y5a7c9e1g3i5k7m9o1q",
+            "name": "BS-urban-11",
+            "ip": "",
+            "port": "",
+            "position": "[121.654321,25.123456]",
+            "description": "bs-11",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "0j2l4n6p8r0t2v4x6z8b"
+              }
+            ],
+            "status": 2,
+            "gNBId": 11,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-11 15:12:49.420805",
+            "lastoff": null
+          },
+          {
+            "id": "2r4t6v8x0z2b4d6f8h0j",
+            "name": "BS-rural-12",
+            "ip": "",
+            "port": "",
+            "position": "[120.876543,24.345678]",
+            "description": "bs-12",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "1k3m5o7q9s1u3w5y7a9c"
+              }
+            ],
+            "status": 1,
+            "gNBId": 12,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-12 12:57:03.309704",
+            "lastoff": null
+          },
+          {
+            "id": "1k3m5o7q9s1u3w5y7a9c",
+            "name": "BS-coastal-13",
+            "ip": "",
+            "port": "",
+            "position": "[121.432109,24.567890]",
+            "description": "bs-13",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "2l4n6p8r0t2v4x6z8b0d"
+              }
+            ],
+            "status": 2,
+            "gNBId": 13,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-13 09:34:28.198603",
+            "lastoff": null
+          },
+          {
+            "id": "0d2f4h6j8l0n2p4r6t8v",
+            "name": "BS-mountain-14",
+            "ip": "",
+            "port": "",
+            "position": "[120.765432,23.678901]",
+            "description": "bs-14",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "3m5o7q9s1u3w5y7a9c1e"
+              }
+            ],
+            "status": 1,
+            "gNBId": 14,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-14 18:23:51.087502",
+            "lastoff": null
+          },
+          {
+            "id": "9w1y3a5c7e9g1i3k5m7o",
+            "name": "BS-island-15",
+            "ip": "",
+            "port": "",
+            "position": "[121.210987,22.987654]",
+            "description": "bs-15",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "4n6p8r0t2v4x6z8b0d2f"
+              }
+            ],
+            "status": 2,
+            "gNBId": 15,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-15 11:09:36.976401",
+            "lastoff": null
+          },
+          {
+            "id": "8p0r2t4v6x8z0b2d4f6h",
+            "name": "BS-metro-16",
+            "ip": "",
+            "port": "",
+            "position": "[121.901234,25.345678]",
+            "description": "bs-16",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "5o7q9s1u3w5y7a9c1e3g"
+              }
+            ],
+            "status": 1,
+            "gNBId": 16,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-16 14:45:19.865300",
+            "lastoff": null
+          },
+          {
+            "id": "7i9k1m3o5q7s9u1w3y5a",
+            "name": "BS-suburban-17",
+            "ip": "",
+            "port": "",
+            "position": "[121.098765,24.789012]",
+            "description": "bs-17",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "6p8r0t2v4x6z8b0d2f4h"
+              }
+            ],
+            "status": 2,
+            "gNBId": 17,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-17 06:31:54.754209",
+            "lastoff": null
+          },
+          {
+            "id": "6b8d0f2h4j6l8n0p2r4t",
+            "name": "BS-industrial-18",
+            "ip": "",
+            "port": "",
+            "position": "[120.543210,23.901234]",
+            "description": "bs-18",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "7q9s1u3w5y7a9c1e3g5i"
+              }
+            ],
+            "status": 1,
+            "gNBId": 18,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-18 19:17:40.643108",
+            "lastoff": null
+          },
+          {
+            "id": "5u7w9y1a3c5e7g9i1k3m",
+            "name": "BS-commercial-19",
+            "ip": "",
+            "port": "",
+            "position": "[121.789012,25.012345]",
+            "description": "bs-19",
+            "bstype": 2,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "8r0t2v4x6z8b0d2f4h6j"
+              }
+            ],
+            "status": 2,
+            "gNBId": 19,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-19 08:53:25.532007",
+            "lastoff": null
+          },
+          {
+            "id": "4n6p8r0t2v4x6z8b0d2f",
+            "name": "BS-hillside-20",
+            "ip": "",
+            "port": "",
+            "position": "[121.456789,24.567890]",
+            "description": "bs-20",
+            "bstype": 1,
+            "components": [
+              {
+                "type": "cu+du+ru",
+                "id": "9s1u3w5y7a9c1e3g5i7k"
+              }
+            ],
+            "status": 1,
+            "gNBId": 20,
+            "gNBIdLength": 22,
+            "adminstate": 1,
+            "laston": "2024-04-20 15:41:37.421906",
+            "lastoff": null
           }
         ]
       }
